c9b83b6ffcd657d9854558d18e2e538ff3065cef
Bayram ARSLAN <by.ram1789@gmail.com>
2023-02-05 22:49:03 +0300

    Girilen değer ile elmas oluşturma

---
+++ yildizUcgen/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen bir sayı giriniz : ");
+        int number = input.nextInt();
+        int a = 1, tempNumber = number;
+        while(a > 0){
+            for(int i = 1; i<=number; i++){
+                for(int j = 1; j<=(number-i); j++){
+                    System.out.print(" ");
+                }
+                for(int k=1; k<=(2*i)-1; k++){
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+            for(int x = 1; x<=number; x++){
+                for(int y = 0; y<=(x-1); y++){
+                    System.out.print(" ");
+                }
+                for(int z= 3; z<=(2*tempNumber)-1; z++){
+                    System.out.print("*");
+                }
+                tempNumber --;
+                System.out.println(" ");
+            }
+            a--;
+        }
+    }
+}
